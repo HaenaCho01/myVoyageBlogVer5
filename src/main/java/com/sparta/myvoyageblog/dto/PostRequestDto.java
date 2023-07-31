ff1c52b6ddbf48ed0b0299ed705a235fe01404e1
Haena <johanna0123@naver.com>
2023-07-31 22:41:09 +0900

    7/31 - Controller 테스트 코드 작성하기
    MockMvc 를 사용해서 Controller 테스트 코드를 작성해주세요!

--- src/main/java/com/sparta/myvoyageblog/dto/PostRequestDto.java
+++ src/main/java/com/sparta/myvoyageblog/dto/PostRequestDto.java
@@ -1,9 +1,19 @@
 package com.sparta.myvoyageblog.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class PostRequestDto {
     private String title;
     private String content;
+
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public PostRequestDto(@JsonProperty("title") String title, @JsonProperty("content") String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
---
+++ src/test/java/com/sparta/myvoyageblog/mvc/PostControllerTest.java
@@ -0,0 +1,109 @@
+package com.sparta.myvoyageblog.mvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.myvoyageblog.config.WebSecurityConfig;
+import com.sparta.myvoyageblog.controller.PostController;
+import com.sparta.myvoyageblog.controller.UserController;
+import com.sparta.myvoyageblog.dto.PostRequestDto;
+import com.sparta.myvoyageblog.entity.User;
+import com.sparta.myvoyageblog.entity.UserRoleEnum;
+import com.sparta.myvoyageblog.security.UserDetailsImpl;
+import com.sparta.myvoyageblog.service.PostServiceImpl;
+import com.sparta.myvoyageblog.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.security.Principal;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(
+		controllers = {UserController.class, PostController.class},
+		excludeFilters = {
+				@ComponentScan.Filter(
+						type = FilterType.ASSIGNABLE_TYPE,
+						classes = WebSecurityConfig.class
+				)
+		}
+)
+
+//@SpringBootTest
+@TestPropertySource(locations = "classpath:test.properties")
+@AutoConfigureMockMvc
+class PostControllerTest {
+
+	@Autowired
+	MockMvc mvc;
+
+	@Autowired
+	private WebApplicationContext context;
+
+	@Autowired
+	ObjectMapper mapper;
+
+	@MockBean
+	PostServiceImpl postServiceImpl;
+
+	@MockBean
+	UserService userService;
+
+	private Principal mockPrincipal;
+
+	private static final String BASE_URL = "/api";
+
+	@BeforeEach
+	public void setup() {
+		mvc = MockMvcBuilders.webAppContextSetup(context)
+				.apply(springSecurity(new MockSpringSecurityFilter()))
+				.build();
+	}
+
+	private void mockUserSetup() {
+		// Mock 테스트 유져 생성
+		String username = "sollertia4351";
+		String password = "robbie1234";
+		UserRoleEnum role = UserRoleEnum.USER;
+		User testUser = new User(username, password, role);
+		UserDetailsImpl testUserDetails = new UserDetailsImpl(testUser);
+		mockPrincipal = new UsernamePasswordAuthenticationToken(testUserDetails, "", testUserDetails.getAuthorities());
+	}
+
+	@Test
+	@DisplayName("게시글 저장 테스트")
+	void save_test() throws Exception {
+		//given
+		this.mockUserSetup();
+		String title = "Test title";
+		String content = "Test content";
+
+		PostRequestDto requestDto = new PostRequestDto(title, content);
+		String postInfo = mapper.writeValueAsString(requestDto);
+
+		//when - then
+		mvc.perform(post(BASE_URL + "/posts")
+						.content(postInfo) //HTTP Body에 데이터를 담는다
+						.contentType(MediaType.APPLICATION_JSON) //보내는 데이터의 타입을 명시
+						.accept(MediaType.APPLICATION_JSON) // 받는 데이터의 타입을 명시
+						.principal(mockPrincipal)
+				)
+				.andExpect(status().isOk())
+				.andDo(print());
+	}
+}
